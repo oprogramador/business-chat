bbeef53a9efdbc104db89d16d8c87970e707c86b
oprogramador <uomodislesia@gmail.com>
2016-11-01 22:49:27 +0100

    lint and connecting to arango

---
+++ backend/app/config.js
@@ -0,0 +1,8 @@
+export default {
+  db: {
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+  },
+};
--- backend/app/servicesManager.js
+++ backend/app/servicesManager.js
@@ -1,8 +1,20 @@
+import arangojs from 'arangojs';
 import bunyan from 'bunyan';
+import config from 'config';
+import packageInfo from '../package';
+import url from 'url';
 
-const packageInfo = require('../package');
 const logger = bunyan.createLogger({ name: packageInfo.name });
+const db = arangojs({
+  url: url.format({
+    auth: `${config.db.user}:${config.db.password}`,
+    hostname: config.db.host,
+    port: config.db.port,
+    protocol: 'http',
+  }),
+});
 
 export {
+  db,
   logger,
 };
