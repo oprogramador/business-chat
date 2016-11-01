42dacfedf4aca4d940221112efaae98ca5383b0d
oprogramador <uomodislesia@gmail.com>
2016-11-01 23:47:39 +0100

    message scenarios

---
+++ backend/app/tests/integration/model/Message.js
@@ -0,0 +1,8 @@
+describe('Message', () => {
+  it('saves and retrieves object when it is valid');
+  it('invalidates when authentication fails');
+  it('invalidates when there are missing fields');
+  it('invalidates when there are additional fields');
+  it('invalidates when provided room id does not exist');
+  it('invalidates when provided message is not a string');
+});
