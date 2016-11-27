92774688672c9074e23bd9a6655a85314759f528
oprogramador <uomodislesia@gmail.com>
2016-11-27 22:58:43 +0100

    Team model

---
+++ backend/app/tests/unit/model/Team.js
@@ -0,0 +1,35 @@
+import Team from 'business-chat-backend/model/Team';
+import ValidationError from 'business-chat-backend/errors/ValidationError';
+import expect from 'business-chat-backend/tests/expect';
+import sinon from 'sinon';
+
+let sandbox;
+
+describe('Team', () => {
+  beforeEach('create sandbox', () => {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach('restore sandbox', () => {
+    sandbox.restore();
+  });
+
+  it('throws ValidationError when provided team name is not a string', () => {
+    expect(() => new Team({ name: 123 })).to.throw(ValidationError);
+  });
+
+  it('throws ValidationError when provided team name is an empty string', () => {
+    expect(() => new Team({ name: '' })).to.throw(ValidationError);
+  });
+
+  it('succeeds when provided team name is valid', () => {
+    expect(() => new Team({ name: 'foo' })).to.not.throw(Error);
+  });
+
+  it('calls `validate` with the same arguments', () => {
+    const validate = sandbox.spy(Team.prototype, 'validate');
+    const object = { name: 'bar' };
+    new Team(object);
+    expect(validate.withArgs(object)).to.be.calledOnce();
+  });
+});
