019450e0fbc6588a90bffb9a34e100d0e098fbd0
oprogramador <uomodislesia@gmail.com>
2016-12-04 01:51:57 +0100

    update js-abstract-synchronizer

--- backend/app/services/serializer.js
+++ backend/app/services/serializer.js
@@ -1,6 +1,6 @@
 import Message from 'business-chat-backend/model/Message';
 import Room from 'business-chat-backend/model/Room';
-import SerializerFactory from 'js-abstract-synchronizer';
+import { SerializerFactory } from 'js-abstract-synchronizer';
 import Team from 'business-chat-backend/model/Team';
 import User from 'business-chat-backend/model/User';
 import { db } from 'business-chat-backend/servicesManager';
